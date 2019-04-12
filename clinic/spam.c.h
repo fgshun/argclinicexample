24d99821c45bfff02f90d4157b60fb518995c72f
fgshun <fgshun@gmail.com>
2019-04-13 01:54:58 +0900

    for CPython 3.8

--- clinic/spam.c.h
+++ clinic/spam.c.h
@@ -9,7 +9,7 @@ PyDoc_STRVAR(spam_add__doc__,
 "add a to b");
 
 #define SPAM_ADD_METHODDEF    \
-    {"add", (PyCFunction)spam_add, METH_FASTCALL|METH_KEYWORDS, spam_add__doc__},
+    {"add", (PyCFunction)(void(*)(void))spam_add, METH_FASTCALL|METH_KEYWORDS, spam_add__doc__},
 
 static PyObject *
 spam_add_impl(PyObject *module, PyObject *a, PyObject *b);
@@ -19,17 +19,20 @@ spam_add(PyObject *module, PyObject *const *args, Py_ssize_t nargs, PyObject *kw
 {
     PyObject *return_value = NULL;
     static const char * const _keywords[] = {"a", "b", NULL};
-    static _PyArg_Parser _parser = {"OO:add", _keywords, 0};
+    static _PyArg_Parser _parser = {NULL, _keywords, "add", 0};
+    PyObject *argsbuf[2];
     PyObject *a;
     PyObject *b;
 
-    if (!_PyArg_ParseStackAndKeywords(args, nargs, kwnames, &_parser,
-        &a, &b)) {
+    args = _PyArg_UnpackKeywords(args, nargs, NULL, kwnames, &_parser, 2, 2, 0, argsbuf);
+    if (!args) {
         goto exit;
     }
+    a = args[0];
+    b = args[1];
     return_value = spam_add_impl(module, a, b);
 
 exit:
     return return_value;
 }
-/*[clinic end generated code: output=b33220affa0a7eed input=a9049054013a1b77]*/
+/*[clinic end generated code: output=c7fbcf1866c352e1 input=a9049054013a1b77]*/
